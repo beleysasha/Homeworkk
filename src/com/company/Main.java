9199a9a1506d472c93f089504931d8c8e732901e
Alex Beley <sasha.school.2005@gmail.com>
2020-06-24 23:02:40 +0300

    added modifief file main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,31 +3,28 @@ import java.util.Scanner;
 public class Main {
 static Scanner scr = new Scanner(System.in);
     public static void main(String[] args) {
-        int array[] = new int[]{scr.nextInt(),scr.nextInt(),scr.nextInt(),scr.nextInt(),scr.nextInt()};
-        int minValue=array[0];
+        int array[] = new int[scr.nextInt()];
         int maxValue=array[0];
+        int minValue=array[0];
         int sumOfEven=0;
         int sumOfNotEven = 0;
+
+
         for (int i = 0;i<array.length;i++){
+            array[i]= scr.nextInt();
             if(maxValue<array[i]){
                 maxValue = array[i];
             }
-        }
-        for (int i = 0;i<array.length;i++){
-            if(minValue>array[i]){
-                minValue = array[i];
+            else if(minValue>array[i]){
+                minValue=array[i];
             }
         }
         for (int i = 0;i<array.length;i++){
             if(i%2==0){
                 sumOfEven +=array[i];
 
-            }
-        }
-        for (int i = 0;i<array.length;i++){
-            if(i%2!=0){
+            }else{
                 sumOfNotEven +=array[i];
-
             }
         }
         System.out.println("min value of array is " + minValue + "\n"
