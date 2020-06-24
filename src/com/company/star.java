7b59d1e81c5f1582fea6191278636e9e8d02e774
Alex Beley <sasha.school.2005@gmail.com>
2020-06-24 21:54:01 +0300

    added star.java

---
+++ src/com/company/star.java
@@ -0,0 +1,25 @@
+package com.company;
+public class star {
+
+   public int r;
+   public double f;
+
+    public static void main(String[] args){
+        int r = 0;
+        for (int i=1;i<8;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+    public star(int r){
+
+    }
+    public star(double f){
+
+    }
+
+}
