57956236fd272ed625eb634bb47289188208b348
Alex Beley <sasha.school.2005@gmail.com>
2020-06-24 22:20:45 +0300

    added converse triangle

--- src/com/company/star.java
+++ src/com/company/star.java
@@ -8,6 +8,13 @@ public class star {
             }
             System.out.println();
         }
+        System.out.println();
+        for (int i=8;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 
     }
