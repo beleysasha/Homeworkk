4c4c00e23565e35a5d2f0e351a3096fe63296364
Alex Beley <sasha.school.2005@gmail.com>
2020-06-24 21:58:29 +0300

    modified star.java file

--- src/com/company/star.java
+++ src/com/company/star.java
@@ -1,9 +1,6 @@
 package com.company;
 public class star {
 
-   public int r;
-   public double f;
-
     public static void main(String[] args){
         int r = 0;
         for (int i=1;i<8;i++){
@@ -13,12 +10,6 @@ public class star {
             System.out.println();
         }
 
-
-    }
-    public star(int r){
-
-    }
-    public star(double f){
 
     }
 
